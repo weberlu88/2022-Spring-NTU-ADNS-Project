d2a7ee9c3ba8a67299f5f85bc94dfe238779fa37
WeberLu <43847259+weberlu88@users.noreply.github.com>
2022-03-12 22:32:03 +0800

    <feat> 留言板排版完成

--- frontend/src/components/Forum.jsx
+++ frontend/src/components/Forum.jsx
@@ -1,7 +1,48 @@
+import { Row, Col, Input, Button } from 'antd';
+const { TextArea } = Input;
+
 export default function Formun() {
-    return (
-      <main style={{ padding: "1rem 0" }}>
-        <h2>Forum</h2>
-      </main>
-    );
-  }
+  // https://ant.design/components/input/#header
+  // state = {
+  //   value: '',
+  // };
+
+  // onChange = ({ target: { value } }) => {
+  //   this.setState({ value });
+  // };
+
+  return (
+
+    <main style={{ padding: "1rem 0" }}>
+      <h2>Forum</h2>
+
+      <div style={{ width: '40%' }}>
+        <Row>
+          <Col span={6} style={{ fontWeight: 'bold' }}>暱稱</Col>
+          <Col span={18}><Input placeholder="匿名使用者" /></Col>
+        </Row>
+        <Row>
+          <Col span={6} style={{ fontWeight: 'bold' }}>留言</Col>
+          <Col span={18}>
+            <TextArea
+              // value={value}
+              // onChange={this.onChange}
+              placeholder="Leave some messages"
+              autoSize={{ minRows: 2, maxRows: 3 }}
+            />
+          </Col>
+        </Row>
+        <Row gutter={[16, 24]}>
+          <Col span={3} offset={21}>
+            <Button id="btn_comment" type="primary" style={{ float: 'right' }}>Send</Button>
+          </Col>
+        </Row>
+
+        {/* 留言顯示區 */}
+        <div>
+
+        </div>
+      </div>
+    </main >
+  );
+}
