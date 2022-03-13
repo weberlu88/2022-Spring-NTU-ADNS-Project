271ca159d2f3d7d62a5f2714ed115f6462052fd0
WeberLu <43847259+weberlu88@users.noreply.github.com>
2022-03-13 12:28:36 +0800

    <feat> 關於我 新增圖片

--- frontend/src/components/PersonalProfile.jsx
+++ frontend/src/components/PersonalProfile.jsx
@@ -3,15 +3,20 @@ import { Card } from 'antd';
 import { Row, Col } from 'antd';
 import { Typography } from 'antd';
 import { Layout } from 'antd';
+import { Image } from 'antd';
 const { Title, Paragraph, Text, Link } = Typography;
 const { Header, Content, Footer, Sider } = Layout;
+
+import img from '../images/miko_ride.jpg';
 
 class PersonalProfile extends React.Component {
   render() {
     return (
       <Card title="About me" style={{ width: '50%' }}>
-        <Row>
-          <Col span={6}> image </Col>
+        <Row gutter={10}>
+          <Col span={6}>
+            <Image src={img} />
+          </Col>
           <Col span={18}>
             <Title level={3}>呂晟維</Title>
             <p>大家好! 我來自台大資管所的ANTSlab，大學畢業於中央資管，雖然大家都說孫媽很恐怖，但偷偷告訴你，其實孫媽不恐怖而是超~級~恐~怖~。還有，孫媽她上碩班選修課的時候好親切，有種反差萌的感覺，想到不吧。</p>
